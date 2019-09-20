80dbbb14da454d2fc8e83a8fe1e3b69d7ff44bc4
Radi Danchev <rd@ammuse.com>
2019-09-20 12:28:06 +0300

    Style cards on home page

--- src/components/Card/Cardslist.js
+++ src/components/Card/Cardslist.js
@@ -2,19 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Cardslist = props => {
-  const { children } = props;
+  const { children, horizontal } = props;
 
   return (
     <React.Fragment>
-      <div className="cardslist">{children}</div>
+      <div className={horizontal ? "cardslist-horizontal" : "cardslist"}>{children}</div>
 
       {/* --- STYLES --- */}
       <style jsx>{`
-        .cardslist {
+        .cardslist,
+        .cardslist-horizontal {
           text-align: center;
         }
 
         @from-width desktop {
+          .cardslist-horizontal {
+            display: flex;
+            flex-wrap: wrap;
+            align-items: baseline;
+          }
           .cardslist {
             display: flex;
             flex-direction: column;
@@ -27,7 +33,12 @@ const Cardslist = props => {
 };
 
 Cardslist.propTypes = {
+  horizontal: PropTypes.bool,
   children: PropTypes.node.isRequired
+};
+
+Cardslist.defaultProps = {
+  horizontal: false
 };
 
 export default Cardslist;
