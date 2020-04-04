5ca646a2b9b26ae4930d0917be21cf14ffd133ca
Patrick <39915222+paddy420Smokers@users.noreply.github.com>
2020-04-04 17:02:32 +0300

    Update html.js

--- src/html.js
+++ src/html.js
@@ -19,6 +19,13 @@ module.exports = class HTML extends React.Component {
     return (
       <html {...this.props.htmlAttributes}>
         <head>
+       <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K97JQLG');</script>
+<!-- End Google Tag Manager -->
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -41,6 +48,10 @@ module.exports = class HTML extends React.Component {
           <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
         </head>
         <body {...this.props.bodyAttributes}>
+          <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K97JQLG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
           <noscript>You need to enable JavaScript to run this app!</noscript>
           {this.props.preBodyComponents}
           <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
